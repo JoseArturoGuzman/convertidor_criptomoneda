f4dd44bc229dbf13a42c77c25d90bb814d2d5b06
Jose Arturo Guzman <125223327+JoseArturoGuzman@users.noreply.github.com>
2025-01-09 07:35:08 -0400

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:convertidor_criptomoneda/screens/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark(). copyWith(
+        primaryColor: Colors.lightBlue,
+        scaffoldBackgroundColor: Colors.white
+      ),
+
+      home: HomeScreen()
+    );
+  }
+}
